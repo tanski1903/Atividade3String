d19cc0b7f795ecfbdc825d4c9a377440fbd80666
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-08 19:58:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Atividade 3 Função String</title>
+</head>
+<body>
+    <Form action="atividade3.php" method="post">
+        <h1>Separador de data</h1>
+        <br><br>
+        <label for="dt">insira a Data:</label>
+        <br><br>
+        <input type="text" id="dt" name="dt"/>
+        <br><br>
+        <input type="submit" value="Separar">
+    </Form>
+   
+    
+</body>
+</html>
